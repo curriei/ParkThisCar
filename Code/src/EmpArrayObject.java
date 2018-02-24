97903bad62a95f688a83326719659409df63bc6a
arifh1 <36606190+arifh1@users.noreply.github.com>
2018-02-24 01:04:09 -0500

    Add files via upload
    To split masterlist into a series of separate lists

---
+++ Code/src/EmpArrayObject.java
@@ -0,0 +1,15 @@
+
+public class EmpArrayObject {
+	
+	int n;
+	int[] A = new int[n];
+	
+	
+	public EmpArrayObject(int n) { //n being the length of the master array
+		
+		for( int i=0; i<n; i++ )
+			  arr[i] = new A();
+	}
+	
+
+}
