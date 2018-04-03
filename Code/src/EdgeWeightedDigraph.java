77984c8da453ea97d2c2c9fed9314315038dcc53
Ian Currie <curriei@mcmaster.ca>
2018-04-02 23:46:40 -0400

    A lot of small stuff

--- Code/src/EdgeWeightedDigraph.java
+++ Code/src/EdgeWeightedDigraph.java
@@ -5,21 +5,46 @@ public class EdgeWeightedDigraph {
 	private final int V;
 	private int E;
 	private LinkedList<DirectedEdge>[] adj;
+	private Coordinate[] coords;
+	private HashST<Coordinate, Integer> ints;
 
-	public EdgeWeightedDigraph(int V) {
-		this.V = V;
+	public EdgeWeightedDigraph(int v) {
+		this.V = 0;
 		this.E = 0;
-		adj = (LinkedList<DirectedEdge>[]) new LinkedList[V];
-		for (int v = 0; v < V; v++)
-			adj[v] = new LinkedList<DirectedEdge>();
+		this.coords = new Coordinate[v];
+		this.ints = new HashST<>(v);
+		adj = (LinkedList<DirectedEdge>[]) new LinkedList[v];
+		for (int w = 0; w < v; w++)
+			adj[w] = new LinkedList<DirectedEdge>();
 	}
 	public int V() { return V; } 
 	public int E() { return E; }
-	public void addEdge(DirectedEdge e) {
+	public boolean contains(int v) { return coords[v]!=null; }
+	public boolean contains(Coordinate c){ return ints.contains(c); }
+	public void addEdge(DirectedEdge e, Coordinate from, Coordinate to) {
 		int v = e.from(), w = e.to();
+		coords[v] = from;
+		coords[w] = to;
+		ints.put(from, v);
+		ints.put(to, w);
 		adj[v].add(e);
 		adj[w].add(e);
 		E++;
+	}
+	public int findClosestIntersection(Coordinate x){
+		Double lowestDist = Double.POSITIVE_INFINITY;
+		int lowIndex = 0;
+		for(int i = 0;i<coords.length; i++){
+			Coordinate y = coords[i];
+			if(y != null){
+				Double dist = x.dist(y);
+				if(dist < lowestDist){
+					lowestDist = dist;
+					lowIndex = i;
+				}
+			}
+		}
+		return lowIndex;
 	}
 	public Iterable<DirectedEdge> adj(int v) { return adj[v]; }
 	
--- Code/src/FileIO.java
+++ Code/src/FileIO.java
@@ -61,58 +61,45 @@ public class FileIO {
     }
         
 	
-	public static Graph importGraph(String fileName) throws IOException{
+	public static EdgeWeightedDigraph importGraph(String fileName) throws IOException{
 		String[][] file = importCSV(fileName); //reads csv file as a 2d array
-		BSTbag bg = new BSTbag();
-		LinkedList<Edge> lst = new LinkedList<>();
+		HashST<Coordinate, Integer> bg = new HashST<>(file.length);
+		Coordinate[] coordBg = new Coordinate[file.length];
+		LinkedList<DirectedEdge> lst = new LinkedList<>();
 		int numNodes = 0;
-		Stack<Edge> edges = new Stack<Edge>();
+		Stack<DirectedEdge> edges = new Stack<DirectedEdge>();
 		for(int i = 1; i<file.length; i++){
 			String[] line = file[i];
-			for(String item :line)
-				System.out.print(item+ "; ");
-			System.out.println();
-			int streetType = Integer.parseInt(line[6]);
-			if(streetType < 9){
-				int from = Integer.parseInt(line[0]);  //from intersection
-				int to = Integer.parseInt(line[1]);  //to intersection
-				int speed = streetType == 4 ? 5280 : 3080;  //matches speed to ft/min
-				String stName = line[24];  //street name
-				Double stWeight = Double.parseDouble(line[36])/speed; //weight mesured in time
-	
-				//all of the below is literally JUST to get the address range
-				Double fromAddress1 = Double.parseDouble(line[16]); 
-				Double toAddress1 = Double.parseDouble(line[17]);
-				Double fromAddress2 = Double.parseDouble(line[18]);
-				Double toAddress2 = Double.parseDouble(line[19]);
-				
-				Double minAddressFrom = Math.min(fromAddress1, fromAddress2);
-				Double maxAddressFrom = Math.max(fromAddress1, fromAddress2);
-				
-				Double addressTo = Math.max(toAddress1, toAddress2);
-				Double addressFrom = minAddressFrom == 0 ? maxAddressFrom : minAddressFrom;
-				
-				String addTo = addressTo.toString()+" "+stName;
-				String addFrom = addressFrom.toString()+" "+stName;
-				
-				int toNode, fromNode;
-				if(!bg.contains(addTo)){
-					bg.add(addTo,numNodes);
-					toNode = numNodes++;
-				}
-				else toNode = (int) bg.get(addTo);
-				if(!bg.contains(addFrom)){
-					bg.add(addFrom, numNodes);
-					fromNode = numNodes++;
-				}
-				else fromNode = (int) bg.get(addFrom);
-				Edge edge = new Edge(toNode, fromNode,stWeight);
-				edges.push(edge);
+			int from = Integer.parseInt(line[0]);
+			int to = Integer.parseInt(line[1]);
+			Double speed = Integer.parseInt(line[2]) == 4 ? 5280. : 3080.;
+			String[] fromCoord = line[3].split(",");
+			String[] toCoord = line[4].split(",");
+			Coordinate fromCo = new Coordinate(Double.parseDouble(fromCoord[0]),
+					Double.parseDouble(fromCoord[1]));
+			Coordinate toCo = new Coordinate(Double.parseDouble(toCoord[0]),
+					Double.parseDouble(toCoord[1]));
+			Double stWeight = fromCo.dist(toCo)/speed;
+			int toNode, fromNode;
+			if(!bg.contains(fromCo)){
+				bg.put(fromCo,numNodes);
+				fromNode = numNodes++;
 			}
+			else fromNode = bg.get(fromCo);
+			if(!bg.contains(toCo)){
+				bg.put(toCo,  numNodes);
+				toNode = numNodes++;
+			}
+			else toNode = bg.get(toCo);
+						
+			DirectedEdge edge = new DirectedEdge(toNode, fromNode,stWeight);
+			edges.push(edge);
 		}
-		Graph graph = new Graph(numNodes);
+		
+		EdgeWeightedDigraph graph = new EdgeWeightedDigraph(numNodes);
 		for(int i = 0; i < numNodes; i++){
-			graph.addEdge((Edge) edges.pop());
+			DirectedEdge e = edges.pop();
+			graph.addEdge(e,coordBg[e.from()],coordBg[e.to()]);
 		}
 		return graph;
 	}
