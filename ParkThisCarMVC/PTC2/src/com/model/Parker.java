969f69b8f79f3369ee83d9655562ff1133e11de3
Ian Currie <curriei@mcmaster.ca>
2018-04-11 19:22:04 -0400

    THIS ACTUALLY WORKS NOW

--- ParkThisCarMVC/PTC2/src/com/model/Parker.java
+++ ParkThisCarMVC/PTC2/src/com/model/Parker.java
@@ -8,8 +8,8 @@ public class Parker{
 	private EdgeWeightedDigraph g;
 	
 	public Parker(){
-		ParkingSpot[] p = FileIO.importList("../../data/Street_parking_sign.txt");
-		EdgeWeightedDigraph g = FileIO.importGraph("../../data/Street_Network_Database.txt");
+		ParkingSpot[] p = FileIO.importList("PTC2/src/data/Street_parking_sign.txt");
+		EdgeWeightedDigraph g = FileIO.importGraph("PTC2/data/data/Street_Network_Database.txt");
 		
 	}
 	
@@ -21,6 +21,12 @@ public class Parker{
 		int start = g.findClosestIntersection(cnew);
 		
 		DijkstraSP d = new DijkstraSP(g,start);
+		
+		Double[] dist = new Double[p.length];
+		for(int i = 0; i<p.length; i++)
+			dist[i] = p[i].getDist(c);
+		
+		QuickSort.sort(dist,p, 0, p.length);
 		
 		Coordinate[] cs = new Coordinate[10];
 		Double[] ds = new Double[10];
@@ -50,55 +56,6 @@ public class Parker{
 		}
 	return out; 
 	}
-	/*
-		public static ArrayList<String> mainFunc(String lat3, String lon3, String day) throws Exception{
-			//line 6 runs with a small dataset, try it with the full dataset though to see if there are any issues
-			ParkingSpot[] p = FileIO.importList("/Users/ibrahimmalik/2XB3/ParkThisCarMVC/ParkThisCar2/Street_parking_sign.txt");
-			
-			//line 9 there is an issue (looks to be with the bag class, have a look, let me know if there are any troubles...)
-			EdgeWeightedDigraph g = FileIO.importGraph("/Users/ibrahimmalik/2XB3/ParkThisCarMVC/ParkThisCar2/Street_Network_Database.txt");
-			
-			
-			//everything else below appears to be fine, there will be more once we have the code from me and ibrahim...
-			Coordinate c = new Coordinate(Double.parseDouble(lat3),
-					Double.parseDouble(lon3));
-			int start = g.findClosestIntersection(c);
-			
-			DijkstraSP d = new DijkstraSP(g,start);
-			
-			Coordinate[] cs = new Coordinate[10];
-			Double[] ds = new Double[10];
-			for(int i = 0; i<10;i++){
-				int vertex = g.findClosestIntersection(p[i].loc());
-				cs[i] = g.coord(vertex);
-				ds[i] = d.getDist(vertex);
-			}
-				
-			QuickSort.sort(ds,p,0,9);
-				
-			int today = Integer.parseInt(day);
-			int i = 0;
-			ArrayList<String> out = new ArrayList<>();
-			while(i<5 && i < p.length){
-				if(p[i].freeToday(today)){
-					//String a = "Parking spot available at: " + p[i].loc().toString()+
-						//	" Available from: " + p[i].sttme() + " to " + p[i].endtme();
-					double a2 =  p[i].loc().getLat();
-					double b2 =  p[i].loc().getLongit();
-					//String place = GOOGLEAPI.Location(a2, b2);
-					String a3 ="Parking spot available at: " + ". Or if you prefer ultimate accuracy:  " + p[i].loc().toString() + ".  Available from: " + p[i].sttme() + " to " + p[i].endtme() + ". ";
-							//" Available from: " + p[i].sttme() + " to " + p[i].endtme();
-					//out.add(a);
-					//System.out.println(a);
-					//i++;
-					out.add(a3);
-					System.out.println(a3);
-					i++;
-					
-				}
-			}
-		return out;	 
-	}*/
 }
 		
 	 
